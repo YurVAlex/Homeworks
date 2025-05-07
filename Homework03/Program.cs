754d7c33c7be3bd3a1af1312307019c7e18abaf0
Yura-va@tut.by <Yura-va@tut.by>
2025-05-07 16:05:01 +0300

    Task1 completed

--- Homework03/Program.cs
+++ Homework03/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Homework03
+﻿namespace Homework03;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Task1();
         }
+    }
+
+    static void Task1()
+    {
+        int age;
+
+        Console.WriteLine("Пожалуйста введите ваш возраст:");
+        while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
+        {
+            Console.WriteLine("Ошибка ввода. Введите целое, положительное число:");
+        }
+
+        var result = age switch
+        {
+            >= 0 and < 3 => "Вы младенец.",
+            >= 3 and < 13 => "Вы ребёнок.",
+            >= 13 and < 18 => "Вы подросток.",
+            >= 60 => "Вы пожилой.",
+            _ => "Вы взрослый."
+        };
+
+        result += (age % 2 == 0) ?
+            " Ваш возраст - число чётное." :
+            " Ваш возраст - число нечётное.";
+
+        Console.WriteLine(result);
     }
 }
