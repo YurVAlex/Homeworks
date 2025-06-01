3ddd4f72f6b82ef57c831ca72d55f45f591cb856
Yura-va@tut.by <Yura-va@tut.by>
2025-06-01 21:49:18 +0300

    Task1, Task2 added

---
+++ Homework07/Task1.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework07;
+
+public class Task1
+{
+    public static void Run()
+    {
+        int[] numbers = new int[6];
+
+        Console.WriteLine("Enter 6 integers for the array elements:\n");
+
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Enter element {i + 1}: ");
+            var input = Console.ReadLine();
+
+            if (int.TryParse(input, out var number))
+            {
+                numbers[i] = number;
+            }
+            else
+            {
+                Console.WriteLine("\nIncorrect input. Please enter an integer.\n");
+                i--; 
+            }
+        }
+
+        Console.WriteLine("\nArray elements entered:");
+        foreach (int number in numbers)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+
+        Array.Sort(numbers);
+
+        Array.Reverse(numbers);
+
+        Console.WriteLine("\nArray elements sorted in descending order:\n");
+        foreach (var number in numbers)
+        {
+            Console.Write($"{number} ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("\nPress any key to finish this task...\n");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ Homework07/Task2.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework07;
+
+public class Task2
+{
+    public static void Run()
+    {
+        int[,] matrix = {
+            { 555, 34, -77 },  
+            { 23, 777, -15 },  
+            { -67, 55, 999 }   
+        };
+
+        Console.WriteLine("Two-dimensional array:");
+
+        for (var i = 0; i < matrix.GetLength(0); i++) 
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++) 
+            {
+                Console.Write($"{matrix[i, j]}\t"); 
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nMaximum elements of each row:");
+
+        for (var i = 0; i < matrix.GetLength(0); i++) 
+        {
+            var maxInRow = matrix[i, 0]; 
+
+            for (var j = 1; j < matrix.GetLength(1); j++) 
+            {
+                if (matrix[i, j] > maxInRow)
+                {
+                    maxInRow = matrix[i, j]; 
+                }
+            }
+            Console.WriteLine($"Line {i}: {maxInRow}");
+        }
+
+        Console.WriteLine("\nPress any key to finish this task...");
+        Console.ReadKey();
+    }
+}
