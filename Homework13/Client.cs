ae149dea360751e0200ca0633e6b0d81366c71cc
Yura-va@tut.by <Yura-va@tut.by>
2025-06-24 19:25:28 +0300

    Homework13. Created.

---
+++ Homework13/Client.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework13;
+
+public class Client
+{
+    private string _name;
+
+    private string _id = Guid.NewGuid().ToString();
+
+    public Client(string name, NewsProvider provider, params Category[] categories)
+    {
+        _name = (!string.IsNullOrEmpty(name)) ? name : "Default";
+
+        if (categories.Length == 0)
+        {
+            foreach (var category in (Category[])Enum.GetValues(typeof(Category)))
+            {
+                provider.Subscribe(category, message =>
+                    Console.WriteLine($"Id: {_id}({_name}) receives: {message}"));
+            }
+        }
+
+        foreach (var category in categories)
+        {
+            provider.Subscribe(category, message => 
+                Console.WriteLine($"Id: {_id}({_name}) receives: {message}"));
+        }
+    }
+}
